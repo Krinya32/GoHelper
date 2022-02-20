cabf7f67615af0ddfef5ac17e3bbf15e93c0cdb2
Krinya32 <krinya32@mail.ru>
2022-02-20 22:10:58 +0300

    Добавил 4 и 5 пункты

--- Pointer/main.go
+++ Pointer/main.go
@@ -22,8 +22,22 @@ func main() {
 		fmt.Printf("After initialization type %T and value %v\n", zeroPointer, zeroPointer)
 	}
 
-	// 4.
+	// 4. Разыменование указателя т.е. получение значения: *pointer - возвращает значение, хранимое по адресу
+	var numericValue int = 32
+	var pointerToNumeric *int = &numericValue
 
-	// 5. Тренируемся еще раз
+	fmt.Printf("value is numericValue is %v\nAdress us %v\n", *pointerToNumeric, pointerToNumeric)
+
+	// 5. Cоздание указателей на нулевые значения типов
+	// var zeroVar int
+	// var zeroPoint *int = &zeroVar
+	zeroPoint := new(int) // создает под капотом zeroValue для int и возвращает адрес, где этот 0 хранится
+	fmt.Printf("Value is *zeropointer %v\n Adress is %v\n", *zeroPoint, zeroPoint)
+
+	// 6. Изменение значения хранимого по адресу через указатель
+	zeroPointerToInt := new(int)
+	fmt.Printf("Adress is %v\n and Value in zeroPointToInt is:%v\n", zeroPointerToInt, *zeroPointerToInt)
+	*zeroPointerToInt += 40
+	fmt.Printf("Adress is %v\n and Value in zeroPointToInt is:%v\n", zeroPointerToInt, *zeroPointerToInt)
 
 }
