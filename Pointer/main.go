951f453335e086c57e52ad86249022511a78946c
Krinya32 <krinya32@mail.ru>
2022-02-21 21:43:48 +0300

    Закончили смотреть Поинтеры (Сложна)

--- Pointer/main.go
+++ Pointer/main.go
@@ -40,4 +40,40 @@ func main() {
 	*zeroPointerToInt += 40
 	fmt.Printf("Adress is %v\n and Value in zeroPointToInt is:%v\n", zeroPointerToInt, *zeroPointerToInt)
 
+	b := 345
+	a := &b
+	c := &b
+	*c += 100
+	*a++
+	fmt.Println(b)
+	// 7. Указательная арифметика ОТСУТСТСВУЕТ ПОЛНОСТЬЮ
+	// Адрес одной ячейки - вы можете через этот адрес продвинуться в другие ячейки
+	// cpp_like := "cpp"
+	// cpp_pointer := &cpp_like
+	// cpp_pointer++
+
+	// 8. Передача указателей в функции
+	// Колосальный прирост производительности,, за счет того что передается не значение, которое должно копироваться, а  передается лишь адрес в памяти, за которым уже хранитья значение
+	sample := 1
+	// samplePointer := &sample
+	fmt.Println("Origin value of sample", sample)
+	changeParam(&sample)
+
+	fmt.Println("After sample changing is", sample)
+
+	// 9. Возврат поинтера из функции (В С++ результат такого механизма - не определен впринципе)
+	ptr1 := returnPointer()
+	ptr2 := returnPointer()
+	fmt.Printf("ptr1: %T and adress %v and value %v\nptr2: %T and adress %v and value %v", ptr1, ptr1, *ptr1, ptr2, ptr2, *ptr2)
+}
+
+// 9.1 Инициализация функции вовзращающей указатель
+func returnPointer() *int {
+	var numeric int = 321
+	return &numeric // В момент вовзрата GO перемещает данную переменную в кучу
+}
+
+// 8.1 Определение функции принимающей параметр, как указатель
+func changeParam(val *int) {
+	*val += 100
 }
