189e61a5f79cc95000db0003c03c72852d105760
Krinya32 <krinya32@mail.ru>
2022-02-22 21:46:48 +0300

    Methoods beginner

---
+++ Methoods/Employee_1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	name     string
+	position string
+	salary   int
+	currency string
+}
+
+// 1. Методы - функции привязанные к определнным структурам
+// func (s struct) MethodName(parameters type) type {}
+// Reciever - получатель метода
+
+func (e Employee) DisplayInfo() {
+	fmt.Println("Name:", e.name)
+	fmt.Println("Position:", e.position)
+	fmt.Printf("Salary: %d %s\n", e.salary, e.currency)
+}
+
+func main() {
+	emp := Employee{
+		name:     "Bob",
+		position: "Senior",
+		salary:   3000,
+		currency: "USD",
+	}
+	// 2. Вызов метода
+	emp.DisplayInfo()
+}
---
+++ Methoods/figure_2/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// 3. В чем преимущество методов над функциями?
+// Во первых: наличие методов улучшает консистентность кода, так как напрямую влияет на его понимание
+// Во вторых: в ГО запрещается создавать функции с одинаковыми назнаваниями, а методы разрешены
+
+type Circle struct {
+	radius float64
+}
+
+type Rectangle struct {
+	length, width int
+}
+
+// 4. Создадим функцию и метод Perimeter  для структуры circle
+func Perimeter(c Circle) float64 {
+	return c.radius * 2 * math.Pi
+}
+
+func (c Circle) Perimeter() float64 {
+	return c.radius * 2 * math.Pi
+}
+
+// 5. Создадими функцию и метод Perimeter для структуры Rectangle
+func (r Rectangle) Perimeter() int {
+	return 2 * (r.width + r.length)
+}
+
+// При обьявлении функции Perimeter происходит ошибка
+// В ГО разрешено создавать методы с одинаковыми именами в пределах одной структуры. Главное чтобы получатель метода вразных структурах где реализован метод со схожим именем отличался
+
+func main() {
+	c := Circle{10.5}
+	fmt.Println("Call function:", Perimeter(c))
+	fmt.Println("Call methood:", c.Perimeter())
+
+	r := Rectangle{10, 20}
+	fmt.Println("Call methood", r.Perimeter())
+}
